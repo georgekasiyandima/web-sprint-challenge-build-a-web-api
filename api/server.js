e5fc7c36dcf15e7bcb56bb0952e1eac88a73f33c
George Kasiyandima <kasiyandimageorge@gmail.com>
2023-11-01 15:40:23 +0200

    wip

--- api/server.js
+++ api/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const morgan = require('morgan');
 const server = express();
-
+server.use(express.json()) 
 function customMorgan(req, res, next) {
   console.log(`you made a ${req.method} request`);
   next();
